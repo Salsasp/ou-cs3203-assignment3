40d1e63db5f3c3863a74bbb431f1a7ec3406cb7d
Salsasp <dakotagee99@gmail.com>
2023-09-13 13:33:26 -0500

    main function

--- assignment.cpp
+++ assignment.cpp
@@ -24,5 +24,14 @@ int vectorProduct(vector<int> nums)
 
 int main()
 {
-    vector<int> myList = {1,2,3,4,5};
+    int num1,num2;
+    vector<int> nums;
+    cout << "Enter num 1" << endl;
+    cin >> num1;
+    cout << "Enter num 2" << endl;
+    cin >> num2;
+    nums.push_back(num1);
+    nums.push_back(num2);
+    cout << "Sum: " << vectorSum(nums);
+    cout << "Product: " << vectorProduct(nums);
 }
